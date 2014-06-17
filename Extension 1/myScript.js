c94c924f826d8d57095fd36faab3b96a4487feb9
Ankit Bansal <ankitbansal2901@gmail.com>
2014-06-17 15:16:03 +0530

    Added Tags for extension and remove page option 17.6.14 3:15 pm

--- Extension 1/myScript.js
+++ Extension 1/myScript.js
@@ -1,32 +1,35 @@
 $("document").ready(function(){
 	
 $("#save").click(function(){
+  window.location.href="save.html " ;
   // alert("Clicked");
 // var url="multi";
 //    alert(url);
-     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-   var  murl = tabs[0].url; 
+//      chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+//    var  murl = tabs[0].url; 
 
 
-alert(murl);
-var max=30;//keep this filled and try to use post method on url
 
-  var jxhr= $.ajax({ 
-    type:"POST",
-    url:"http://localhost/pumpapp/Extension 1/save.php",
-    data:"url="+murl,
-    success:function(data){
-      console.log(data);
+// var max=30;//keep this filled and try to use post method on url
 
-    }
-});
+//   var jxh= $.ajax({ 
+//     type:"POST",
+//     url:"http://localhost/pumpapp/Extension 1/save.php",
+//     data:"url="+murl,
+//     success:function(data){
+//       console.log(data);
+
+//     }
+
+// });
+  
 // //doSomething();
 // jxhr.done(function(){alert("Complete");});
-window.location.href="save.html " ;
+
 });
 
 
-});
+
 
 
 $("#shareg").click(function(){
@@ -47,16 +50,45 @@ $("#shareg").click(function(){
 });
 
 
+$("#save_tags").click(function(){
+
+ chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+   var  curl = tabs[0].url;   
+//keep this filled and try to use post method on url
+var tags=$("input").val();
+  var jxhr= $.ajax({ 
+    type:"POST",
+    url:"http://localhost/pumpapp/Extension 1/save_tags.php",
+    data:{"url":curl ,"tags":tags},
+    success:function(data){
+      console.log(data);
+    }
+});
+
+});
+
+window.location.href="save_tags.html";
 });
 
 
+$("#clicked").click(function(){
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+   var  turl = tabs[0].url;   
+//keep this filled and try to use post method on url
+  var jr= $.ajax({ 
+    type:"POST",
+    url:"http://localhost/pumpapp/Extension 1/remove.php",
+    data:"url="+turl ,
+    success:function(data){
+      console.log(data);
+    }
+});
 
+});
+  window.location.href="removed.html";
+});
 
-
-
-
-
-
+});
 // function doSomething() {
 //     $.get("save.php");
 //     return false;
