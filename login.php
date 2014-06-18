3874f9b64dbf8923f48650cf7b0e242d054a2036
Ankit Bansal <ankitbansal2901@gmail.com>
2014-06-18 16:47:58 +0530

    Sign up and log in complete 18.6.14 4:47pm

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
-<!-- <!DOCTYPE html> -->
+<!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8"/>
-  <title>PUMP!- Sign Up</title>
+  <title>PUMP!- Share your URLs-Sign In</title>
 <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 <style type="text/css">
 
@@ -11,13 +11,12 @@
   padding-top:10px;
   padding-bottom: 8px;
   border-bottom: 2px solid gray;
-
+  color: white;
 
 }
       body{
-      	background-color: #f5f5f5;
-      
-      background-image: url('bg.jpg');
+      	text-align: center;
+      background-image: url('edit.jpg');
       background-size: cover;
       background-repeat: no-repeat;
       opacity: 0.9;
@@ -25,25 +24,30 @@
 
     }
 
-      .col{
-      color: white;
-    }
-   
+
       .form-signin { 
-        width: 450px;
+        width: 350px;
         padding: 19px 29px 29px;
         margin: 0 auto 20px;
         background-color: #fff;
         border: 1px solid #e5e5e5;
-       
       }
      
-      
+      .form-signin .form-signin-heading,
+      .form-signin .checkbox {
+        margin-bottom: 10px;
+      }
+      .form-signin input[type="email"],
+      .form-signin input[type="password"] {
+        font-size: 16px;
+        height: auto;
+        margin-bottom: 15px;
+        padding: 7px 9px;
+      }
       .container{
-        text-align: center;
+        position: relative;
+     }
 
-      }
-     
       h1{
         font-family: 'Exo', sans-serif;
       }
@@ -57,13 +61,14 @@ Share your URLs</small></h1>
 </heading>
 <section>
 <div class="container">
-      <form method=POST action="signUp.php" class="form-signin">
-        <h2 class="form-signin-heading">Sign Up!</h2>
+      <form method=POST action="login.php" class="form-signin">
+        <h2 class="form-signin-heading">Sign In!</h2></br>
         <label for="emailID">E-mail ID:</label>
         <input type="email" name ="emailID" placeholder="Email address" required></br>
         <label for="password">Password:</label>
-        <input type="password" name="password"  placeholder="Password" required></br>
-
+        <input type="password" name="password"  placeholder="Password" required></br></br>
+		<button type="submit" name="submit" id="sign_in" class="btn a2 btn-default btn-primary">Sign In</button>
+	
         <button type="submit" name="submit" id="sign_up" class="btn a2 btn-default btn-primary">Sign Up</button>
 	
         </form>
@@ -75,26 +80,30 @@ Share your URLs</small></h1>
 
 
 
+
 <div id="assets">
 
-<script type="text/javascript" src="jquery.js"></script>
-<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-
-
-</div>
 <?php
-if(isset($_POST["submit"])){
+	if(isset($_POST["submit"])){
 $id=$_POST["emailID"];
 $pw=$_POST["password"];
 
-$url="http://166.62.18.107:8080/PumpAppWebsevice/REST/webService/signUpPumpApp;emailID=";
+$url="http://166.62.18.107:8080/PumpAppWebsevice/REST/webService/LoginAuthPumpApp;userEmail=";
 $url.=$id;
 $url.=";password=";
 $url.=$pw;
 $url.=";";
-}
 
+echo $url;
+}
 ?>
+<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+$("#sign_up").click(function(){
+  window.location.href="http://localhost/pumpapp/signUp.php";
+});
+</script>
 <script type="text/javascript">
 <?php if(isset($_POST["submit"])){?> 
 
@@ -109,9 +118,7 @@ $("document").ready(function(){
     url: url,                                                                                                                                              
     dataType: 'jsonp',                                                                                                                                                                                                
     success: function() { console.log('Success!');
-    	$.getJSON(url, null, function () {
-        alert('Cross domain JS call achieved. Have your implementation going in here!');});
-
+    	
      },                                                                                                                                                                                       
     error: function() { console.log('Uh Oh!'); }                                                                                                                           
 })
@@ -119,11 +126,7 @@ $("document").ready(function(){
 
 });
 <?php } ?>
-
 </script>
-
-
+</div>
 </body>
-</html>
-
 </html>
