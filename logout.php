964af201486723fffc0fcc4cd5320b3c346ac346
Ankit Bansal <ankitbansal2901@gmail.com>
2014-07-10 18:36:22 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,9 @@
-<?php
-    require_once("session.php");
-    
-	$ID=$_SESSION["user_id"];
-?>
+<?php require_once("db_connection2.php"); 
+ require_once("session.php");
+ ?>
+
+ <?php 
+ session_destroy();
+ header("location:welcome.php");
+exit(); ?>
 
